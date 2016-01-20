14fe9c606cbe30572053a0329334475999a77dc7
kvapilJiri <george.kvapil@seznam.cz>
2016-01-20 18:12:15 +0100

    Spaces removed. Added recursion.

--- www/install/functions.php
+++ www/install/functions.php
@@ -4,21 +4,25 @@
     {
         $listItems = "";
         foreach($items as $item_name => $item_text){ 
-           $listItems .='<li><span class="label label-danger">'.$item_text.'</span></li>';
-        }    
+            if(is_array($item_text)){
+                $listItems .= createListItems($item_text);
+            } else {
+                $listItems .='<li><span class="label label-danger">'.$item_text.'</span></li>';
+            }
+        }
         return $listItems;
     }
 
     function showErrors(array $errors)
-    {            
+    {
         $error_list = "";
-        if(isset($errors) && !empty($errors)){      
+        if(isset($errors) && !empty($errors)){
              $error_list .= '<ul class="errors">';
-             foreach($errors as $error_in => $error_text){                     
+             foreach($errors as $error_in => $error_text){
                 if(is_array($error_text)) $error_list .= createListItems($error_text);
                 else $error_list .='<li><span class="label label-danger">'.$error_text.'</span></li>';
              }
              $error_list .= '</ul>';
-         }  
+         }
          return $error_list;
     }
